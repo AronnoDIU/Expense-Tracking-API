349d38fca3b0576d0eecac91c7667784a42bc31b
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2025-03-29 15:40:59 +0600

    Expense Tracking API

---
+++ src/main/java/com/aronno/expensetracking_api/ExpenseTrackingApiApplication.java
@@ -0,0 +1,13 @@
+package com.aronno.expensetracking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseTrackingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aronno/expensetracking_api/ExpenseTrackingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aronno.expensetracking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
