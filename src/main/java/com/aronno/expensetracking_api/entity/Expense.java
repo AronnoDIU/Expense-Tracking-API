789e7b915f415fb982747f03f22d279d9145d498
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2025-04-01 20:25:11 +0600

    Accountability:Added

--- src/main/java/com/aronno/expensetracking_api/entity/Expense.java
+++ src/main/java/com/aronno/expensetracking_api/entity/Expense.java
@@ -4,8 +4,11 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.util.Date;
 
 @Data
@@ -30,4 +33,14 @@ public class Expense {
     private String category;
 
     private Date date;
+
+    @SuppressWarnings("JpaDataSourceORMInspection")
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @SuppressWarnings("JpaDataSourceORMInspection")
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private Timestamp updatedAt;
 }
