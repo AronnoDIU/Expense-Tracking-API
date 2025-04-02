b3876584fad28ae4d91390dba5a7ac43dd0213de
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2025-04-02 13:25:32 +0600

    Update:CustomException

--- src/main/java/com/aronno/expensetracking_api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/aronno/expensetracking_api/exceptions/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.sql.Date;
 
@@ -22,5 +23,29 @@ public class GlobalExceptionHandler {
         errorObject.setTimestamp(Date.valueOf(java.time.LocalDate.now()));
 
         return new ResponseEntity<>(errorObject, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorObject> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex, WebRequest request) {
+
+        ErrorObject errorObject = new ErrorObject();
+
+        errorObject.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        errorObject.setMessage("Invalid argument type: " + ex.getValue());
+        errorObject.setTimestamp(Date.valueOf(java.time.LocalDate.now()));
+
+        return new ResponseEntity<>(errorObject, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorObject> handleException(Exception ex, WebRequest request) {
+
+        ErrorObject errorObject = new ErrorObject();
+
+        errorObject.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorObject.setMessage(ex.getMessage());
+        errorObject.setTimestamp(Date.valueOf(java.time.LocalDate.now()));
+
+        return new ResponseEntity<>(errorObject, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
