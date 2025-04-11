aaf9208903dbebcf456a872fcff8074339065d0e
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2025-04-11 18:25:53 +0600

    Modified: JWT Filter

--- src/main/java/com/aronno/expensetracking_api/security/JwtRequestFilter.java
+++ src/main/java/com/aronno/expensetracking_api/security/JwtRequestFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,16 +21,19 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     private final CustomUserDetailsService userDetailsService;
 
     public JwtRequestFilter(
-            JwtTokenUtil jwtTokenUtil,
-            CustomUserDetailsService userDetailsService
+            @NonNull JwtTokenUtil jwtTokenUtil,
+            @NonNull CustomUserDetailsService userDetailsService
     ) {
         this.jwtTokenUtil = jwtTokenUtil;
         this.userDetailsService = userDetailsService;
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
 
         final String requestTokenHeader = request.getHeader("Authorization");
 
