2f5c129cdabf676e3296f5933674e4ea0541fefb
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2025-04-12 08:29:08 +0600

    Modified:RateLimitInterceptor

--- src/main/java/com/aronno/expensetracking_api/config/RateLimitInterceptor.java
+++ src/main/java/com/aronno/expensetracking_api/config/RateLimitInterceptor.java
@@ -4,10 +4,14 @@ import io.github.bucket4j.Bucket;
 import io.github.bucket4j.ConsumptionProbe;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public class RateLimitInterceptor implements HandlerInterceptor {
+    private static final String RATE_LIMIT_REMAINING_HEADER = "X-Rate-Limit-Remaining";
+    private static final String RATE_LIMIT_LIMIT_HEADER = "X-Rate-Limit-Limit";
     private final Bucket bucket;
 
     public RateLimitInterceptor(Bucket bucket) {
@@ -15,16 +19,19 @@ public class RateLimitInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(@NonNull HttpServletRequest request, 
+                           @NonNull HttpServletResponse response, 
+                           @NonNull Object handler) {
         ConsumptionProbe probe = bucket.tryConsumeAndReturnRemaining(1);
         
         if (probe.isConsumed()) {
-            response.addHeader("X-Rate-Limit-Remaining", String.valueOf(probe.getRemainingTokens()));
+            response.setHeader(RATE_LIMIT_REMAINING_HEADER, String.valueOf(probe.getRemainingTokens()));
+            response.setHeader(RATE_LIMIT_LIMIT_HEADER, "100");
             return true;
         }
 
         response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
-        response.addHeader("X-Rate-Limit-Retry-After-Milliseconds", String.valueOf(probe.getNanosToWaitForRefill() / 1_000_000));
+        response.setHeader(HttpHeaders.RETRY_AFTER, String.valueOf(probe.getNanosToWaitForRefill() / 1_000_000_000));
         return false;
     }
 }
