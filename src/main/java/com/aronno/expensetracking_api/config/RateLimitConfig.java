6c85e9c01cfa8c2a0d37fc8dbd75dea85e762e86
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2025-04-12 08:35:20 +0600

    Modified:RateLimitConfig

--- src/main/java/com/aronno/expensetracking_api/config/RateLimitConfig.java
+++ src/main/java/com/aronno/expensetracking_api/config/RateLimitConfig.java
@@ -2,7 +2,6 @@ package com.aronno.expensetracking_api.config;
 
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
-import io.github.bucket4j.Bucket4j;
 import io.github.bucket4j.Refill;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -20,7 +19,12 @@ public class RateLimitConfig implements WebMvcConfigurer {
     }
 
     private Bucket createBucket() {
-        Bandwidth limit = Bandwidth.classic(100, Refill.greedy(100, Duration.ofMinutes(1)));
-        return Bucket4j.builder().addLimit(limit).build();
+        Duration duration = Duration.ofMinutes(1);
+        Refill refill = Refill.greedy(100, duration);
+        Bandwidth limit = Bandwidth.classic(100, refill);
+        
+        return Bucket.builder()
+                .addLimit(limit)
+                .build();
     }
 }
